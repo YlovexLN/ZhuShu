0e200111f976a5ca1244da3410fcd65dd485eb2e
YlovexLN <1584544837@qq.com>
2025-09-25 16:30:04 +0800

    feat(config): 更新网站图标配置选项

--- src/config.ts
+++ src/config.ts
@@ -113,8 +113,8 @@ export const siteConfig: SiteConfig = {
 		// 留空以使用默认 favicon
 		 {
 		   src: 'https://qiniu.ylovexln.top/blog/favicon.png',    // 图标文件路径
-		   theme: 'dark',              // 可选，指定主题 'light' | 'dark'
-		   sizes: '32x32',              // 可选，图标大小
+		   //theme: 'dark',              // 可选，指定主题 'light' | 'dark'
+		   //sizes: '128x128',              // 可选，图标大小
 		 }
 	],
 
