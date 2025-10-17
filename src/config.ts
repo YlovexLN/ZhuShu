30f23de69e9a0ae5f5658a2b4ef3a669592e4322
YlovexLN <1584544837@qq.com>
2025-10-18 00:26:56 +0800

    fix(config): 更新 favicon 路径为相对路径

--- src/config.ts
+++ src/config.ts
@@ -112,7 +112,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		// 留空以使用默认 favicon
 		 {
-		   src: 'https://qiniu.ylovexln.top/blog/favicon-dark-128.png',    // 图标文件路径
+		   src: 'favicon/favicon-dark-128.png',    // 图标文件路径
 		   theme: 'dark',              // 可选，指定主题 'light' | 'dark'
 		   sizes: '128x128',              // 可选，图标大小
 		 }
